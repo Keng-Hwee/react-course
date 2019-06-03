410d95fdaa1dd8f076a7d312ea3c471e11b0a8d9
Keng Hwee <kenghwee.t@gmail.com>
2019-06-04 00:49:33 +0800

    Set up webpack with babel

--- IndecisionApp/src/app.js
+++ IndecisionApp/src/app.js
@@ -1,5 +1,14 @@
 //import "./utils.js";
-import { square, add } from "./utils.js";
+import anythingIWant, { square, add } from "./utils.js";
 console.log("app.js is runninggg");
 console.log(square(2));
 console.log(add(1, 10));
+console.log(anythingIWant(10, 1)); // Note that this is 'subtract' since it is a default, we can name it
+
+import validator from "validator";
+console.log(validator.isEmail("text"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
+ReactDOM.render(template, document.getElementById("app"));
--- IndecisionApp/src/utils.js
+++ IndecisionApp/src/utils.js
@@ -2,7 +2,11 @@ console.log("utils.js is running");
 
 const square = x => x * x;
 const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 //exports: default export OR named export
 
 // named export
-export { square, add };
+export { square, add, subtract as default };
+
+// OR
+// export default (a,b) => a-b

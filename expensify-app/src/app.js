c0391a3ca7cd85acd7064da3d20adad5babe32d0
Keng Hwee <kenghwee.t@gmail.com>
2019-06-10 23:33:30 +0800

    completed redux section

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css"; //abs path looks into node_modules
 import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const store = configureStore();
+
+store.dispatch(
+    addExpense({ description: "Water Bill", amount: 4500, createdAt: 1 })
+);
+store.dispatch(
+    addExpense({ description: "gas bill", amount: 1000, createdAt: 2 })
+);
+store.dispatch(
+    addExpense({ description: "rent", amount: 109500, createdAt: 3 })
+);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
